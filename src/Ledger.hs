c38406ead57c00fe5016dfabb638dbf747d5f752
Harold Carr <harold.carr@gmail.com>
2017-03-27 20:38:11 -0700

    experiment

--- src/Ledger.hs
+++ src/Ledger.hs
@@ -1,10 +1,12 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RankNTypes        #-}
 
 module Ledger
   ( EIndex
   , EHash
   , ETimestamp
   , EData
+  , LedgerOperations (LedgerOperations)
   )
 where
 
@@ -14,3 +16,13 @@ type EIndex     = Int
 type EHash      = ByteString
 type ETimestamp = ByteString
 type EData      = ByteString
+
+class LedgerTC a where
+class LedgerEntryTC a where
+
+data LedgerOperations l e =
+  LedgerOperations
+    -- listEntries : Nothing: return all; Just i: return block at index i
+    (LedgerTC l => Maybe Int   -> IO (Maybe l))
+    -- isValidEntry : Nothing: valid; Just reason: not valid with reason
+    (LedgerEntryTC e => e -> IO (Maybe String))
